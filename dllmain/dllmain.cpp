4b61964c9e709c70098b47a124dcee76a3f2a9e5
kseroff <112875767+kseroff@users.noreply.github.com>
2025-08-26 22:36:48 +0300

    Add files via upload

---
+++ dllmain/dllmain.cpp
@@ -0,0 +1,132 @@
+﻿#include "pch.h"
+#include <tchar.h>
+#include <iostream>
+
+extern "C" {
+
+    class __declspec(dllexport) CompExtended1S {
+    public:
+        TCHAR CPU[250];
+        int RAM;
+        double Cost;
+        TCHAR GPU[250];
+
+        CompExtended1S() {
+            _tcscpy_s(this->CPU, _T(""));
+            this->RAM = 0;
+            this->Cost = 0.0;
+            _tcscpy_s(this->GPU, _T(""));
+        }
+
+        ~CompExtended1S() {}
+
+        void InputCompExtended1() {
+            _tprintf(_T("Enter CPU: "));
+            _tscanf_s(_T("%s"), this->CPU, (unsigned)_countof(this->CPU));
+
+            _tprintf(_T("Enter RAM: "));
+            std::cin >> this->RAM;
+
+            _tprintf(_T("Enter Cost: "));
+            std::cin >> this->Cost;
+
+            _tprintf(_T("Enter GPU: "));
+            _tscanf_s(_T("%s"), this->GPU, (unsigned)_countof(this->GPU));
+        }
+
+        void OutputCompExtended1() {
+            _tprintf(_T("CPU: %s\n"), this->CPU);
+            _tprintf(_T("RAM: %d\n"), this->RAM);
+            _tprintf(_T("Cost: %f\n"), this->Cost);
+            _tprintf(_T("GPU: %s\n"), this->GPU);
+        }
+    };
+
+    class __declspec(dllexport) CompExtended2S : public CompExtended1S {
+    public:
+        TCHAR Brand[250];
+
+        CompExtended2S() : CompExtended1S() {
+            _tcscpy_s(this->Brand, _T(""));
+        }
+        ~CompExtended2S() {}
+
+        void InputCompExtended2() {
+            InputCompExtended1();
+            std::cin.ignore(std::cin.rdbuf()->in_avail());
+            _tprintf(_T("Enter Brand: "));
+            _tscanf_s(_T("%s"), this->Brand, (unsigned)_countof(this->Brand));
+        }
+
+        void OutputCompExtended2() {
+            OutputCompExtended1();
+            std::cin.ignore(std::cin.rdbuf()->in_avail());
+            _tprintf(_T("Brand: %s\n"), this->Brand);
+        }
+    };
+
+    class __declspec(dllexport) CompExtended3S : public CompExtended2S {
+    public:
+        TCHAR AdditionalInfo[250];
+
+        CompExtended3S() : CompExtended2S() {
+            _tcscpy_s(this->AdditionalInfo, _T(""));
+        }
+        ~CompExtended3S() {}
+
+        void InputCompExtended3() {
+            InputCompExtended2();
+            _tprintf(_T("Enter GPU: "));
+            _tscanf_s(_T("%s"), this->AdditionalInfo, (unsigned)_countof(this->AdditionalInfo));
+        }
+
+        void OutputCompExtended3() {
+            OutputCompExtended2();
+            _tprintf(_T("AdditionalInfo: %s\n"), this->AdditionalInfo);
+        }
+    };
+
+}
+
+extern "C" {
+    __declspec(dllexport) void PrintString(const char* str) {
+        std::cout << str << std::endl;
+    }
+
+    __declspec(dllexport) void PrintInt(int num) {
+        std::cout << num << std::endl;
+    }
+
+    __declspec(dllexport) void PrintDouble(double num) {
+        std::cout << num << std::endl;
+    }
+
+    __declspec(dllexport) void ScanString(char* str, int size) {
+        std::cin.getline(str, size);
+    }
+
+    __declspec(dllexport) void ScanInt(int* num) {
+        std::cin >> *num;
+    }
+
+    __declspec(dllexport) void ScanDouble(double* num) {
+        std::cin >> *num;
+    }
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ doroshenko_dll/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace doroshenko_dll
+{
+
+    public class Comp
+    {
+        private string CPU { get; set; }
+        private int RAM { get; set; }
+        private double Cost { get; set; }
+
+        public Comp()
+        {
+            this.CPU = "";
+            this.RAM = 0;
+            this.Cost = 0;
+        }
+
+        public Comp(string CPU, int RAM, double cost)
+        {
+            this.CPU = CPU;
+            this.RAM = RAM;
+            this.Cost = cost;
+        }
+
+        public Comp(Comp d)
+        {
+            this.CPU = d.CPU;
+            this.RAM = d.RAM;
+            this.Cost = d.Cost;
+        }
+
+        [DllImport("dllmain.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern void PrintString(string str);
+
+        [DllImport("dllmain.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern void PrintInt(int num);
+
+        [DllImport("dllmain.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern void PrintDouble(double num);
+
+        [DllImport("dllmain.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern void ScanString(string str, int size);
+
+        [DllImport("dllmain.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern void ScanInt(ref int num);
+
+        [DllImport("dllmain.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern void ScanDouble(ref double num);
+
+        public void Input()
+        {
+            Console.WriteLine("Enter CPU:");
+            string CPU = Console.ReadLine();
+            ScanString(CPU, CPU.Length);
+
+            Console.WriteLine("Enter RAM:");
+            int RAM = int.Parse(Console.ReadLine());
+            ScanInt(ref RAM);
+
+            Console.WriteLine("Enter Cost:");
+            double Cost = double.Parse(Console.ReadLine());
+            ScanDouble(ref Cost);
+
+            this.CPU = CPU;
+            this.RAM = RAM;
+            this.Cost = Cost;
+        }
+
+        public void Output() => Console.WriteLine($"CPU: {CPU}  RAM: {RAM}  Cost: {Cost}");
+
+    }
+
+    class Program
+    {
+
+        [StructLayout(LayoutKind.Sequential,
+         Pack = 4,
+         CharSet = CharSet.Unicode)]
+        public unsafe struct __CompExtended1S
+        {
+            //public IntPtr* _vtable;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 250)]
+            public string CPU;
+
+            public int RAM;
+            public double Cost;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 250)]
+            public string GPU;
+        };
+
+        public unsafe class CompExtended1S : IDisposable
+        {
+            private __CompExtended1S _cpp;
+
+            [DllImport("dllmain.dll", EntryPoint = "??0CompExtended1S@@QAE@XZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern IntPtr _CompExtended1S_Constructor(ref __CompExtended1S ths);
+
+            [DllImport("dllmain.dll", EntryPoint = "??1CompExtended1S@@QAE@XZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern int _CompExtended1S_Destructor(ref __CompExtended1S ths);
+
+            [DllImport("dllmain.dll", EntryPoint = "?InputCompExtended1@CompExtended1S@@QAEXXZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern void _InputCompExtended1(ref __CompExtended1S ths);
+
+            [DllImport("dllmain.dll", EntryPoint = "?OutputCompExtended1@CompExtended1S@@QAEXXZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern void _OutputCompExtended1(ref __CompExtended1S ths);
+
+            public CompExtended1S()
+            {
+                _cpp = new __CompExtended1S();
+                _CompExtended1S_Constructor(ref _cpp);
+            }
+            public void Dispose()
+            {
+                _CompExtended1S_Destructor(ref _cpp);
+            }
+
+            public void InputCompExtended1()
+            {
+                _InputCompExtended1(ref _cpp);
+            }
+
+            public void OutputCompExtended1()
+            {
+                _OutputCompExtended1(ref _cpp);
+            }
+        };
+
+        [StructLayout(LayoutKind.Sequential,
+        //Pack = 4,
+        CharSet = CharSet.Unicode)]
+        public unsafe struct __CompExtended2S
+        {
+            public __CompExtended1S Base;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 250)]
+            public string Brand;
+        };
+
+        public unsafe class CompExtended2S : IDisposable
+        {
+            private __CompExtended2S _cpp2;
+
+            [DllImport("dllmain.dll", EntryPoint = "??0CompExtended2S@@QAE@XZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern IntPtr _CompExtended2S_Constructor(ref __CompExtended2S ths);
+
+            [DllImport("dllmain.dll", EntryPoint = "??1CompExtended2S@@QAE@XZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern int _CompExtended2S_Destructor(ref __CompExtended2S ths);
+
+            [DllImport("dllmain.dll", EntryPoint = "?InputCompExtended2@CompExtended2S@@QAEXXZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern void _InputCompExtended2(ref __CompExtended2S ths);
+
+            [DllImport("dllmain.dll", EntryPoint = "?OutputCompExtended2@CompExtended2S@@QAEXXZ", CallingConvention = CallingConvention.ThisCall)]
+            private static extern void _OutputCompExtended2(ref __CompExtended2S ths);
+
+            public CompExtended2S()
+            {
+                _cpp2 = new __CompExtended2S();
+                _CompExtended2S_Constructor(ref _cpp2);
+            }
+
+            public void Dispose()
+            {
+                _CompExtended2S_Destructor(ref _cpp2);
+            }
+
+            public void InputCompExtended2()
+            {
+                _InputCompExtended2(ref _cpp2);
+            }
+
+            public void OutputCompExtended2()
+            {
+                _OutputCompExtended2(ref _cpp2);
+            }
+        };
+
+
+        static void Main(string[] args)
+        {
+
+            /*           Comp comp = new Comp();
+
+                        comp.Input();
+                        Console.WriteLine();
+                        comp.Output();
+            */
+
+            CompExtended1S sc1 = new CompExtended1S();
+            CompExtended2S sc2 = new CompExtended2S();
+
+            sc1.InputCompExtended1();
+            Console.WriteLine();
+            sc1.OutputCompExtended1();
+            Console.WriteLine();
+
+            sc2.InputCompExtended2();
+            Console.WriteLine();
+            sc2.OutputCompExtended2();
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+
+            Thread.Sleep(200000);
+        }
+    }
+}
